4d3e4654a0dbcfa318d452fb4d2fbe10ed59f14d
efsa@marte <efsa@bath.edu>
2021-11-16 21:39:21 +0100

    Update 10_dataExploration.R

--- 10_dataExploration.R
+++ 10_dataExploration.R
@@ -1,6 +1,7 @@
 # load libraries
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 
 # set-up params
 dataFolder <- 'receivedData_DO_NOT_EDIT/'
@@ -64,13 +65,25 @@ buildingMeterReadingsConsolidated %>%
   filter(building_id %in% 1:16) %>%
   ggplot(aes(x = timestamp, y = meter_multi_reading)) +
   geom_line(aes(colour = building_id)) +
-  facet_wrap(~building_id, scales = 'free')
+  facet_wrap(~building_id, scales = 'free') +
+  theme(legend.title = element_blank())
 
 # import weather dataset
 weatherData <- read.csv(paste0(dataFolder, 'weather_data.csv'))
+weatherData$timestamp <- as.POSIXct(weatherData$timestamp)
 
 # check uniqueness of id, and if all is contained on the metadata
 length(unique(weatherData$site_id))
 sum(unique(weatherData$site_id) %in% buildingMetadata$site_id)
 sum(unique(buildingMetadata$site_id) %in% unique(weatherData$site_id))
 # now I know that building_id maps to metadata, site_id maps to weather data
+
+weatherDataColsToPivot <- colnames(weatherData)[!colnames(weatherData) %in% c('site_id', 'timestamp')]
+weatherDataLong <- weatherData %>%
+  pivot_longer(all_of(weatherDataColsToPivot),
+               names_to = 'variable',
+               values_to = 'value')
+
+
+
+
